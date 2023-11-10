f297167a496abf8b436cc198a0a58e914088e6de
leejinsol234 <desirable7@naver.com>
2023-11-10 17:26:01 +0900

    initial config complete

---
+++ src/main/java/org/board/project/configs/MvcConfig.java
@@ -0,0 +1,45 @@
+package org.board.project.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableConfigurationProperties(FileUploadConfig.class)
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private FileUploadConfig fileUploadConfig;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        //System.out.println("fileUploadConfig : "+fileUploadConfig);
+        //fileUploadConfig : FileUploadConfig(path=C:/uploads/, url=/uploads/)
+
+                                                //업로드 경로를 포함한 모든 경로
+        registry.addResourceHandler(fileUploadConfig.getUrl()+"**")
+                .addResourceLocations("file:///"+fileUploadConfig.getPath());
+    }
+
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        return new HiddenHttpMethodFilter();
+    }
+
+    @Bean
+    public MessageSource messageSource(){
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+
+        ms.setDefaultEncoding("UTF-8");
+        ms.addBasenames("messages.commons","messages.validations","messages.errors");
+
+        return ms;
+    }
+}
